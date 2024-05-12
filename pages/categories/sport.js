85be95ce527907a0feb501d811b233cc48bc07aa
Hasan Ashrafi <yasashrafi@gmail.com>
2024-05-12 12:40:55 +0330

    create sport cars component

---
+++ pages/categories/sport.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import CarsList from '../components/templates/CarsList'
+import carsData from '@/data/carsData'
+
+function Sport() {
+    const sportCars = carsData.filter((car) => car.category === 'sport')
+
+    return <CarsList data={sportCars} />
+}
+
+export default Sport
