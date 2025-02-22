2c70922c37d91bc13c30401410508dfc6f8f98ed
Hasan Ashrafi <yasashrafi@gmail.com>
2025-02-22 18:34:28 +0330

    `Added check for empty data and no cars available message`

--- pages/components/templates/CarsPage.js
+++ pages/components/templates/CarsPage.js
@@ -1,13 +1,16 @@
-
 import React from 'react'
 import Card from '../module/Card'
 
 function CarsPage({ data }) {
+    if (!data || data.length === 0) {
+        return <div>No cars available</div>
+    }
+
     return (
         <div className='flex flex-wrap justify-center mt-10 mb-24'>
             {
-                data.map((car) => (    
-                    <Card key={car.id} {...car}/>   
+                data.map((car) => (
+                    <Card key={car.id} {...car} />
                 ))
             }
         </div>
