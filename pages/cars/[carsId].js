37d3af90344147be6d66f9923d5c0a4184479b32
Hasan Ashrafi <yasashrafi@gmail.com>
2025-02-22 12:47:44 +0330

    `Refactor CarDetail and CarsList components to handle missing or invalid car IDs`

--- pages/cars/[carsId].js
+++ pages/cars/[carsId].js
@@ -2,15 +2,21 @@ import { useRouter } from 'next/router'
 import carsData from '@/data/carsData';
 import CarsDetail from '../components/templates/CarsDetail';
 
-
 function CarDetail() {
-    const router = useRouter();
-    const { carsId } = router.query;
-    const carDetails = carsData[carsId - 1]
-  
-    return <CarsDetail {...carDetails}/>
-       
-    
+  const router = useRouter();
+  const { carsId } = router.query;
+
+  if (!carsId) {
+    return <div>Car not found</div>;
+  }
+
+  const carDetails = carsData[parseInt(carsId) - 1];
+
+  if (!carDetails) {
+    return <div>Car not found</div>;
+  }
+
+  return <CarsDetail {...carDetails} />;
 }
 
 export default CarDetail
--- pages/components/templates/CarsList.js
+++ pages/components/templates/CarsList.js
@@ -2,28 +2,26 @@ import { useRouter } from "next/router";
 import { IoMdArrowRoundBack } from "react-icons/io";
 import Card from "../module/Card";
 
-
 function CarsList({ data }) {
-  console.log(data);
-  const router = useRouter()
-  const backHandler = () => {
-    router.back()
-  }
+  const router = useRouter();
+  const backHandler = () => router.back();
+
+  if (!data) return <div>Loading...</div>;
 
   return (
-    <div className=" h-full w-full">
-      <div onClick={backHandler} className="flex w-fit p-1 cursor-pointer  m-10 rounded-md place-items-center text-2xl ">
-        <IoMdArrowRoundBack className="text-white mr-1  bg-blue-500 p-0.5  rounded-2xl " />
+    <div className="h-full w-full">
+      <div onClick={backHandler} className="flex w-fit p-1 cursor-pointer m-10 rounded-md place-items-center text-2xl">
+        <IoMdArrowRoundBack className="text-white mr-1 bg-blue-500 p-0.5 rounded-2xl" />
         <p>Back</p>
       </div>
 
       <div className="flex flex-wrap justify-center">
-        {
-          data.map((item) => <Card key={item.id} {...item} />)
-        }
+        {data.map((item) => (
+          <Card key={item.id} {...item} />
+        ))}
       </div>
     </div>
-  )
+  );
 }
 
-export default CarsList
+export default CarsList;
