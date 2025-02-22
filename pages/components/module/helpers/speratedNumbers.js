a822c519042ec6291eb908f3c6f64e60fe9b2e43
Hasan Ashrafi <yasashrafi@gmail.com>
2025-02-22 19:00:03 +0330

    `Removed input validation for number parameter in speratedNumbers.js helper function`

--- pages/components/module/helpers/speratedNumbers.js
+++ pages/components/module/helpers/speratedNumbers.js
@@ -1,7 +1,5 @@
 const sp = (number, options = {}) => {
-  if (typeof number !== 'number') {
-    throw new Error('Invalid input: number must be a number type');
-  }
+  
   try {
     const formattedNumber = number.toLocaleString(options);
     return formattedNumber;
